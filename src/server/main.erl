3a9c1f727c415d4c62522979edad462f64854fb7
okayno14 <okayno14@gmail.com>
2022-07-25 11:18:13 +0700

    Добавил структуру папок, настроил проект в ide, добавил readme.md.

---
+++ src/server/main.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author aleksandr_work
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. июль 2022 10:42
+%%%-------------------------------------------------------------------
+-module(main).
+-author("aleksandr_work").
+
+%% API
+-export([start/0]).
+
+start() -> io:format("Hello, World! ~n").

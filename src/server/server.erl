20bd58a091abe56f3c0c8d5d9c6640948ddca721
okayno14 <okayno14@gmail.com>
2022-07-26 11:05:22 +0700

    Первый скелет сервера

--- src/server/server.erl
+++ src/server/server.erl
@@ -10,12 +10,30 @@
 -author("aleksandr_work").
 
 %% API
--export([start/0]).
+-export([start/0, wait_request/1]).
 
 start() ->
   case  gen_tcp:listen(1234,[{active, false}]) of
-    {ok, _ListenSocket}->start_servers(8);
+    {ok, ListenSocket}->
+      io:format("~w Acceptors will spawn~n",[8]),
+      start_servers(8,ListenSocket),
+      %%замораживает listen-процесс
+      timer:sleep(infinity);
     {error, Reason}->io:format("Error, can't listen port ~w~n", [Reason])
   end.
 
-start_servers(Num)-> io:format("~w Acceptors will spawn~n",[Num]).
+start_servers(0,_)-> ok;
+start_servers(Num, ListenSocket)->
+  spawn(?MODULE,wait_request,[ListenSocket]),
+  io:format("Acceptor ~w spawned~n",[Num]),
+  start_servers(Num-1,ListenSocket).
+
+wait_request(ListenSocket)->
+  case gen_tcp:accept(ListenSocket) of
+    {ok, SendSocket} -> handle_request(SendSocket);
+    {error, Reason}->io:format("Error, can't accept request. ~w~n",[Reason])
+  end.
+
+handle_request(_SendSocket)->
+  io:format("Request accepted~n").
+

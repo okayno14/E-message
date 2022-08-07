1944d70d20e18361d89a7c0fd47675626356a770
okayno14 <okayno14@gmail.com>
2022-08-07 19:51:43 +0700

    Поправил модель данных. Переписал метод записи диалога в базу: JSON-представление коллекции ников указывается как null, но список ников пишется в отдельное множество. Таким образом экономится память базы.

--- src/repo/redis/redis_dialogue.erl
+++ src/repo/redis/redis_dialogue.erl
@@ -14,22 +14,17 @@
 -export([write/2,
         read/2]).
 
+%%записать пользователей
 write(Con,#dialogue{users = Nicks} = Dialogue)->
   {ok, DID} = eredis:q(Con,["INCR", "SeqDial"]),
-  Commited = Dialogue#dialogue{id=DID},
+  %%Специально зануляю коллекцию ников, так как эта информация будет храниться в множестве
+  Commited = Dialogue#dialogue{id=DID, users = undefined},
   {ok,_} = eredis:q(Con,["HSET",atom_to_list(dialogue),DID,?record_to_json(dialogue,Commited)]),
+  write_users(Con, Commited, Nicks),
+  Dialogue#dialogue{id = binary_to_integer(DID)}.
 
-  eredis:q(Con,["MULTI"]),
-  lists:map(
-    fun(Nick)->
-      Str = string:concat(binary_to_list(Nick),":dialogue"),
-      io:format("~p~n",[Str]),
-      eredis:q(Con,["SADD", Str, DID])
-    end,
-    Nicks),
-  {ok,_} = eredis:q(Con,["EXEC"]),
-  Commited.
-
+%%прочитать пользователей
+%%прочитать сообщения
 read(Con, DID)->
   {ok, T} = eredis:q(Con,["HGET",atom_to_list(dialogue),DID]),
   case T of
@@ -38,3 +33,23 @@ read(Con, DID)->
       [?json_to_record(dialogue,T)]
   end.
 
+%%переписать сообщения
+%%переписать пользователей
+
+%%update(Con, Dialogue)->
+%%  %%message_write
+%%
+
+
+%%DID - бинарная строка, в которой записан численный ID
+write_users(Con,#dialogue{id=DID}, Nicks)->
+  eredis:q(Con,["MULTI"]),
+  _B=[":"|atom_to_list(user)],
+  Str=[atom_to_list(dialogue)|[":"|[DID |_B]]],
+  io:format("~p~n",[Str]),
+  lists:map(
+  fun(Nick)->
+    eredis:q(Con,["SADD", Str, Nick])
+  end,
+  Nicks),
+  {ok,_} = eredis:q(Con,["EXEC"]).

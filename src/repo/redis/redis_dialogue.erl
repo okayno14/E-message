784e0790ccfe805e28a64d5b5b45367f2016760d
okayno14 <okayno14@gmail.com>
2022-08-07 16:32:08 +0700

    Поиск диалога по ID

--- src/repo/redis/redis_dialogue.erl
+++ src/repo/redis/redis_dialogue.erl
@@ -11,10 +11,19 @@
 -include("jsonerl/jsonerl.hrl").
 
 %% API
--export([write/2]).
+-export([write/2,
+        read/2]).
 
 write(Con,Dialogue)->
   {ok, DID} = eredis:q(Con,["INCR", "SeqDial"]),
   Commited = Dialogue#dialogue{id=DID},
   {ok,_} = eredis:q(Con,["HSET",atom_to_list(dialogue),DID,?record_to_json(dialogue,Commited)]),
   Commited.
+
+read(Con, DID)->
+  {ok, T} = eredis:q(Con,["HGET",atom_to_list(dialogue),DID]),
+  case T of
+    undefined->[];
+    _ ->
+      [?json_to_record(dialogue,T)]
+  end.

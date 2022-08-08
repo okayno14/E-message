340d5310942abff76f0f97c5b7a3fddc3b1e2bc8
okayno14 <okayno14@gmail.com>
2022-08-08 12:56:49 +0700

    Написал функцию записи нового сообщения

--- src/repo/redis/redis_message.erl
+++ src/repo/redis/redis_message.erl
@@ -7,7 +7,15 @@
 %%% Created : 07. авг. 2022 10:21
 %%%-------------------------------------------------------------------
 -module(redis_message).
--author("aleksandr_work").
+-include("entity.hrl").
+-include("jsonerl/jsonerl.hrl").
 
 %% API
--export([]).
+-export([write/2]).
+
+write(Con,#message{}= Message)->
+  {ok,MID}=eredis:q(Con,["INCR","SeqMsg"]),
+  Commited = Message#message{id = MID},
+  {ok,_}=eredis:q(Con,["HSET",atom_to_list(message),MID,?record_to_json(message,Commited)]),
+  Commited.
+

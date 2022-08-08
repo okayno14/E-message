5ca51e9ffda24b227df881cbe3adb6dacb848b59
okayno14 <okayno14@gmail.com>
2022-08-08 13:03:05 +0700

    Написал репозиторий сообщений

--- src/repo/redis/redis_message.erl
+++ src/repo/redis/redis_message.erl
@@ -11,11 +11,25 @@
 -include("jsonerl/jsonerl.hrl").
 
 %% API
--export([write/2]).
+-export([write/2,
+        read/2,
+        update/2,
+        delete/2]).
 
-write(Con,#message{}= Message)->
+write(Con,#message{}=Message)->
   {ok,MID}=eredis:q(Con,["INCR","SeqMsg"]),
-  Commited = Message#message{id = MID},
+  Commited = Message#message{id = binary_to_integer(MID)},
   {ok,_}=eredis:q(Con,["HSET",atom_to_list(message),MID,?record_to_json(message,Commited)]),
   Commited.
 
+read(Con,MID) when MID =/= -1 ->
+  {ok,JSON} = eredis:q(Con,["HGET",atom_to_list(message),MID]),
+  [?json_to_record(message,JSON)].
+
+update(Con,#message{id = MID}=Message)->
+  {ok,_}=eredis:q(Con,["HSET",atom_to_list(message),MID]),
+  Message.
+
+delete(Con,#message{id=MID})->
+  {ok,_} = eredis:q(Con,["HDEL",atom_to_list(message),MID]),
+  ok.
